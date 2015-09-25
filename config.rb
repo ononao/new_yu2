88b95c6fd8171d31e223cd726477e86f90e2630f
ononao <naonao230410@gmail.com>
2015-09-25 15:21:03 +0900

    練習中

---
+++ config.rb
@@ -0,0 +1,85 @@
+###
+# Compass
+###
+compass_config do |config|
+ config.output_style = :compressed
+ config.line_comments = false
+end
+
+###
+# Directory
+###
+# activate :directory_indexes
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+ activate :livereload
+
+ activate :autoprefixer, browsers: ['last 3 version', 'ie 7', 'ie 8', 'ie 9']
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+###
+# Gem
+###
+
+
+set :css_dir, 'assets/css'
+set :js_dir, 'assets/js'
+set :images_dir, 'assets/img'
+set :fonts_dir, 'assets/font'
+
+# Build-specific configuration
+configure :build do
+ activate :autoprefixer, browsers: ['last 3 version', 'ie 7', 'ie 8', 'ie 9']
+
+ # For example, change the Compass output style for deployment
+ activate :minify_css
+
+ # Minify Javascript on build
+ activate :minify_javascript
+
+ # Enable cache buster
+ # activate :asset_hash
+
+ # Use relative URLs
+ activate :relative_assets
+ set :relative_links, true
+
+ # Or use a different image path
+ # set :http_prefix, "/Content/images/"
+end
